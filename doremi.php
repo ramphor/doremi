1219bfbf71381c80e3b3556400c738540c9869eb
Puleeno Nguyen <puleeno@gmail.com>
2020-03-27 00:40:49 +0700

    Define plugin info

---
+++ doremi.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Doremi
+ * Plugin URI: https://ramphor.com
+ * Author: Ramphor Premium
+ * Author: https://ramphor.com
+ * Version: 1.0.0
+ */
+
+define( 'DOREMI_PLUGIN_FILE', __FILE__ );
